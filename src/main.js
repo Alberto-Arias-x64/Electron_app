153cc304c4bc36a16cc967a52e77ed2840beacb8
x64 <alberto.arias.x64@gmail.com>
2023-01-18 11:04:17 -0500

    update project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences:{
+            //preload: path.join(__dirname, 'preload.js'),
+            nodeIntegration: true
+        }
     })
 
-    win.loadFile('index.html')
+    win.loadFile('ui/index.html')
 }
 
 app.whenReady().then(() => {
